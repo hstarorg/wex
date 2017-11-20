f19832e6a83f3949d03b74ce69efce0967aba4b4
Hstarorg <hm910705@163.com>
2017-11-20 13:58:46 +0800

    Update demo

--- packages/wex-demo/src/pages/home/home.ts
+++ packages/wex-demo/src/pages/home/home.ts
@@ -1,4 +1,34 @@
 import { Page } from 'wex-core';
 
-export class HomePage extends Page {
+export default class HomePage extends Page {
+  data = {
+    a: 1,
+    b: 'acc',
+    name: 'xxx',
+    age: 1
+  };
+
+  private abc = '999';
+
+  onLoad() {
+    console.log('页面加载');
+    this.test();
+  }
+
+  onShow() {
+    console.log('on Show', this.data.a);
+    setInterval(() => {
+      this.data.name = Math.random().toString(16) + '---------' + this.abc;
+    }, 1000);
+  }
+
+  test() {
+    this.data.age = Math.floor(Math.random() * 100);
+    this.abc =  Math.floor(Math.random() * 1000).toString();
+    this.$$vm.setData({})
+  }
+
+  handleClick(evt: any) {
+    this.test();
+  }
 }
