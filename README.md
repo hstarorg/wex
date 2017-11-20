c12b1666a91166f40a10f63f421ee29c18c0c0f2
Hstarorg <hm910705@163.com>
2017-11-20 13:58:40 +0800

    Optimize two-way binding

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # wex
 The wechat micro program extension tools
+
+# 如何开发wex?
+
+```bash
+# 编译Demo
+cd packages/wex-demo
+wex build -w
+
+# 在微信开发者工具中打开小程序(dist)目录，注意，请务必要在项目详情中，取消勾选ES6转ES5，代码上传时自动压缩
+
+# 编译Core核心项目
+cd ../packages/wex-core
+npm run dev
+
+```
--- packages/wex-core/src/utils/util.ts
+++ packages/wex-core/src/utils/util.ts
@@ -1,24 +1,93 @@
+const LIFECYCLE_HOOKS = [
+  'onLoad',
+  'onReady',
+  'onShow',
+  'onHide',
+  'onUnload',
+  'onPullDownRefresh',
+  'onReachBottom',
+  'onShareAppMessage',
+  'onPageScroll'
+];
 export const util = {
   getConfigObject(obj: any, excludeKeys?: string[]): object {
-    const config = {};
+    const config: any = {};
     // 保证excludeKeys是数组
     !Array.isArray(excludeKeys) && (excludeKeys = []);
     excludeKeys.push('constructor', '$$getOptions');
+
     // 先原型
-    const proto = obj.constructor.prototype;
+    const proto = Object.getPrototypeOf(obj);
     Object.keys(proto).forEach(k => {
       if (excludeKeys.indexOf(k) >= 0) {
         return;
       }
-      config[k] = proto[k];
+      // 只需要绑定生命周期钩子
+      if (LIFECYCLE_HOOKS.indexOf(k) >= 0) {
+        // 在onLoad中拦截vm对象
+        if (k === 'onLoad') {
+          config[k] = function (...args: any[]) {
+            obj.$$vm = this;
+            // 继续调用
+            proto[k].apply(obj, args);
+          }
+        } else {
+          // 其他的钩子函数，直接绑定
+          config[k] = proto[k].bind(obj);
+        }
+      } else if (typeof proto[k] === 'function') {
+        // 其他Function，直接绑定
+        config[k] = proto[k].bind(obj);
+      }
     });
     // 后实例
-    Object.keys(obj).forEach(k => {
-      if (excludeKeys.indexOf(k) >= 0) {
-        return;
-      }
-      config[k] = obj[k];
-    });
+    // Object.keys(obj).forEach(k => {
+    //   if (excludeKeys.indexOf(k) >= 0) {
+    //     return;
+    //   }
+    //   config[k] = obj[k];
+    // });
+    config.data = (Object as any).assign({}, obj.data);
+    // 启用双向绑定
+    this.makeReactive(obj.data, obj);
     return config;
+  },
+  makeReactive(data: any, root: any) {
+    if (!data) {
+      return;
+    }
+    Object.keys(data).forEach(k => {
+      this.defineReactive(data, k, data[k], root);
+    });
+    return data;
+  },
+  defineReactive(obj: object, key: string, value: any, root: any) {
+    var property = Object.getOwnPropertyDescriptor(obj, key);
+    var getter = property && property.get;
+    var setter = property && property.set;
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      get() {
+        return getter ? getter.call(obj) : value;
+      },
+      set(newVal) {
+        // 先取出当前值
+        var val = getter ? getter.call(obj) : value;
+        // 值一样，直接return
+        if (newVal === val || (newVal !== newVal && val !== val)) {
+          return;
+        }
+        // 把值设置回去
+        if (setter) {
+          setter.call(obj, newVal);
+        } else {
+          value = newVal;
+        }
+        root.$$vm.setData({
+          [key]: newVal
+        });
+      }
+    })
   }
 };
